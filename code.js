f91635ed64603f8c7b89ca9b0469b7ca5518513c
davejang <djunkaroo621@users.noreply.github.com>
2019-08-17 17:17:26 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,5 +1,11 @@
-var time = new Date();
-document.write(time);
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+
+document.getElementById("hours").innerHTML = h - 12;
+document.getElementById("minutes").innerHTML = m;
+document.getElementById("seconds").innerHTML = s;
 
 var demo = document.getElementById("header-title");
-demo.innerHTML ="Changed the Content Again";
+demo.innerHTML ="Digital Clock";
