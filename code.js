b70776d8ebcb37c388f96836aa872916b40beea8
davejang <djunkaroo621@users.noreply.github.com>
2019-08-19 01:24:39 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,52 +1,35 @@
+// Assigning variables for the native Javascript functions
 var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 var s = d.getSeconds();
+var monthVar = d.getMonth();
+var day = d.getDate();
+var year = d.getUTCFullYear();
 
 
 document.getElementById("hours").innerHTML = h;
+document.getElementById("minutes").innerHTML = m;
+document.getElementById("seconds").innerHTML = s;
+document.getElementById("month").innerHTML = monthVar;
+document.getElementById("day").innerHTML = day;
+document.getElementById("year").innerHTML = year;
 
 setInterval(timer1, 1000);
 
 function timer1() {
-document.getElementById("hours").innerHTML = h++;
-if (h > 24) {
-  h = 1;
+
+document.getElementById("hours").innerHTML = new Date().getHours();
+document.getElementById("minutes").innerHTML = new Date().getMinutes();
+document.getElementById("seconds").innerHTML = new Date().getSeconds();
+
+document.getElementById("month").innerHTML = new Date().getMonth();
+document.getElementById("day").innerHTML = new Date().getDate();
+document.getElementById("year").innerHTML = new Date().getUTCFullYear();
+
 }
-}
-
-document.getElementById("hours").innerHTML = m;
-
-setInterval(timer2, 1000);
-
-function timer2() {
-document.getElementById("minutes").innerHTML = m++;
-if (m > 60) {
-  m = 0;
-}
-}
-
-
-document.getElementById("seconds").innerHTML = s;
-
-setInterval(timer3, 1000);
-
-function timer3() {
-document.getElementById("seconds").innerHTML = s++;
-if (s > 60) {
-  s = 0;
-}
-}
-
-setInterval(timer4, 1000);
-
-function timer4() {
 if (h >= 12) {
-  document.getElementById("mornOrNight").innerHTML = "PM";
-} else if (h < 12 || h === 24) {
-  document.getElementById("mornOrNight").innerHTML = "AM";
-}
-}
-
-var demo = document.getElementById("header-title");
-demo.innerHTML ="Digital Clock";
+    document.getElementById("mornOrNight").innerHTML = "PM";
+  } else if (h < 12 || h === 24) {
+    document.getElementById("mornOrNight").innerHTML = "AM";
+  }
